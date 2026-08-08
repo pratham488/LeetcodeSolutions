67cf004a244dc451e20b011b9a2212dbc4c79329
pratham488 <mouryapratham52@gmail.com>
2026-08-08 20:21:16 +0530

    Time: 1 ms (43.03%), Space: 10 MB (37.92%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> current;
+    void getallSubsets(vector<int>& nums, vector<int>& ans, int i,
+                       vector<vector<int>>& allSubsets) {
+        
+        if (i == nums.size()) {
+            allSubsets.push_back(ans);
+            return;
+        }
 
-        backtrack(0, nums, current, ans);
+        // Include nums[i]
+        ans.push_back(nums[i]);
+        getallSubsets(nums, ans, i + 1, allSubsets);
 
-        return ans;
+        // Exclude nums[i]
+        ans.pop_back();
+        getallSubsets(nums, ans, i + 1, allSubsets);
     }
 
-    void backtrack(int start, vector<int>& nums,
-                   vector<int>& current,
-                   vector<vector<int>>& ans) {
-        
-        ans.push_back(current);
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
 
-        for (int i = start; i < nums.size(); i++) {
-            current.push_back(nums[i]);
+        getallSubsets(nums, ans, 0, allSubsets);
 
-            backtrack(i + 1, nums, current, ans);
-
-            current.pop_back();
-        }
+        return allSubsets;
     }
 };
